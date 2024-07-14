1125a4c383091dffe15ec6ee2ba44db056f4fb89
kareemrogers-coder <kareemrogers27@gmail.com>
2024-07-14 16:55:40 -0400

    update for me

--- welcome.py
+++ welcome.py
@@ -4,3 +4,11 @@ students = ["james", "Kareem", "Brian", "Zabdiel", "Pamauk"]
 
 for student in students:
     print (student + " is going to crush next week")
+
+# update for me
+
+def birthday_generator(age):
+    new_age = age + 1
+    return f"You just had a birthday, you are now {new_age} years old"
+print(birthday_generator(16))
+print(birthday_generator(29))
