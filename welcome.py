25504d88e7da53a82a315322c9ee9a2bac7504c4
kareemrogers-coder <kareemrogers27@gmail.com>
2024-07-14 16:34:54 -0400

    first commit

---
+++ welcome.py
@@ -0,0 +1,6 @@
+# Hey this some cool coding stuff i did on sunday
+
+students = ["james", "Kareem", "Brian", "Zabdiel", "Pamauk"]
+
+for student in students:
+    print (student + " is going to crush next week")
